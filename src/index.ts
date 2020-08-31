f2a2268a0c7a414faafddab922085bc45a4723f1
Matthew Guo <mg2239@cornell.edu>
2020-08-31 02:02:51 -0400

    added prettier, fixed index file

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
-const dotenv = require('dotenv');
-const discord = require('discord.js');
+import dotenv from 'dotenv';
+import discord from 'discord.js';
 
 const client = new discord.Client();
 
 dotenv.config();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', (msg) => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
+client.on('ready', () => console.log(`Logged in as ${client.user!.tag}!`));
 
 client.login(process.env.BOT_TOKEN);
